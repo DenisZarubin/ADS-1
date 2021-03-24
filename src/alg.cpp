5a4ec8bb36b95dbb53de9613282218aabc006753
DenisZarubin <79105479+DenisZarubin@users.noreply.github.com>
2021-03-24 23:42:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,20 +1,20 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
- int middle, left = 0, right = size - 1, n = 0;
- while (left < right) {
-  middle = left + (right - left) / 2;
-  if (arr[middle] == value) {
-   while (arr[middle-1] == value) middle--;
-   while (arr[middle] == value) middle++, n++;
-   return n;
-  }
-  else if (arr[middle] > value) {
-   right = middle;
-  }
-       else {
+  int middle, left = 0, right = size - 1, n = 0;
+  while (left < right) {
+   middle = left + (right - left) / 2;
+   if (arr[middle] == value) {
+     while (arr[middle-1] == value) middle--;
+     while (arr[middle] == value) middle++, n++;
+     return n;
+    }
+   else if (arr[middle] > value) {
+     right = middle;
+   }
+        else {
    left = middle+1;
-  }
+   }
  }
  return 0; // если ничего не найдено
 }
